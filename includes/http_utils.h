4df8ba8d21b226842efa2534d016d279d0abbc13
Fgp910 <f.gutierrezpena910@gmail.com>
2021-03-05 00:24:47 +0100

    Inicia la definición de algunas funciones auxiliares para el servidor HTTP/1.1

---
+++ includes/http_utils.h
@@ -0,0 +1,8 @@
+#ifndef HTTP_UTILS_H
+#define HTTP_UTILS_H
+
+const char *get_mime(char *filename);
+
+int get_size(char *filename);
+
+#endif
---
+++ src/http_utils.c
@@ -0,0 +1,9 @@
+#include "http_utils.h"
+
+const char *get_mime(char *filename) {
+
+}
+
+int get_size(char *filename) {
+
+}
